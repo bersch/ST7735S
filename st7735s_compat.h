15d162b308b33a075fc8a0297dab0351afa3d299
Bernhard Schneider <bernhard@neaptide.org>
2019-09-30 09:59:22 +0200

    minor

--- st7735s_compat.h
+++ st7735s_compat.h
@@ -27,13 +27,4 @@ void SPI_TransmitData(uint16_t len, uint8_t *data);
 void SPI_Transmit(uint16_t len, uint8_t *data);
 void _Delay(uint32_t d);
 
-
-#include "stm32f3xx_ll_gpio.h"
-#ifndef _Pin_Init
-#define _Pin_Init(name) LL_GPIO_SetPinMode(name ## _GPIO_Port, name ## _Pin, LL_GPIO_MODE_OUTPUT)
-#define _Pin_Toggle(name) LL_GPIO_TogglePin(name ## _GPIO_Port, name ## _Pin)
-#define _Pin_High(name) LL_GPIO_SetOutputPin(name ## _GPIO_Port, name ## _Pin);
-#define _Pin_Low(name) LL_GPIO_ResetOutputPin(name ##_GPIO_Port, name ## _Pin);
-#endif
-
 #endif
