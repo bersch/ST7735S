dc7c8e910b0ee4bdf42231ad5100a1d17f2f25b7
Bernhard Schneider <bernhard@neaptide.org>
2023-06-21 13:53:23 +0200

    minor

--- utils/mkfonts.sh
+++ utils/mkfonts.sh
@@ -4,7 +4,7 @@ rm -f ../fonts.c ../fonts.h
 
 cd fonts
 
-/bin/ls -1  *.bdf|while read f; do echo $f;../bdf2c.pl ' -~' "$f"; done
+/bin/ls -1  *.bdf|while read f; do echo $f;../bdf2c.pl ' -~' 'a-z' 'A-Z' "$f"; done
 /bin/ls -1  *.h  |while read f; do cat "$f" >> ../../fonts.h; done
 /bin/ls -1  *.c  |while read f; do cat "$f" >> ../../fonts.c; done
 
